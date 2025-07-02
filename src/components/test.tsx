58b8ba95bb7b1d5ea61dfe90db35e0f5dcbcee81
createhb21 <alsghk9701@gmail.com>
2025-07-03 02:00:20 +0900

    feat: hello world

---
+++ src/components/test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react'
+
+import App from './App'
+
+describe('<App />', () => {
+  it('should render the App', () => {
+    const { container } = render(<App />)
+
+    expect(
+      screen.getByRole('heading', {
+        name: /Welcome!/i,
+        level: 1
+      })
+    ).toBeInTheDocument()
+
+    expect(screen.getByText(/This is a Quack./i)).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('link', {
+        name: /start building for free/i
+      })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img')).toBeInTheDocument()
+
+    expect(container.firstChild).toBeInTheDocument()
+  })
+})
